d996596f50be9bf932d0ef8c4fcae05607dda53c
IOCloudFly <gusumobai@163.com>
2019-11-02 14:30:13 +0800

    横向诗词

---
+++ lib/net/http_utils.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'dart:async';
+
+
+class HttpUtils{
+
+  static Dio dio;
+
+  static String token;
+
+  static const String API_POETRY = 'https://v2.jinrishici.com/token';
+
+  static const int CONNECT_TIMEOUT = 10000;  //请求超时
+  static const int RECEIVE_TIMEOUT = 3000;   //响应超时
+
+  static const String GET = 'get';
+
+  static const String POST = 'post';
+
+
+
+
+
+
+
+
+
+}
+
--- lib/pages/poetry_page.dart
+++ lib/pages/poetry_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_phoenix/tools/vertical_text.dart';
 
 class PoetryPage extends StatefulWidget {
   @override
@@ -10,7 +12,51 @@ class _PoetryPageState extends State<PoetryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('诗词'),
+        child: new Card(
+          elevation: 6,
+          color: Colors.white,
+          child: Container(
+            color: Colors.transparent,
+            width: 300.0,
+            height: 500.0,
+            child: Column(
+
+              children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: new ClipOval(
+                    child: new SizedBox(
+                      width: 200.0,
+                      height: 200.0,
+                      child: new Image.asset(
+                        "assets/image_01.png",
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: CustomPaint(
+                          painter: VerticalText(
+                              text: "剑气纵横三万里,一剑光寒十九州。",
+                              textStyle: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                                letterSpacing: 6,
+                                wordSpacing: 4,
+
+                              ),
+                            width: 50.0,
+                            height: 180.0,
+                          ),
+                      ),
+                    ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
